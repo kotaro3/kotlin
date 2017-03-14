b8af736c9aea063d79bafc790ae029b14e3efc43
kotaro <k.sato@st-ventures.jp>
2017-03-14 23:06:07 +0900

    [wip] Learning basic syntax

--- app/src/main/java/jp/cy_world/kotaro/kotlintestapp/MainActivity.kt
+++ app/src/main/java/jp/cy_world/kotaro/kotlintestapp/MainActivity.kt
@@ -2,11 +2,41 @@ package jp.cy_world.kotaro.kotlintestapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 動的に型付けるよ
+        var helloWorld = "aaaaa"
+        var switch = 1;
+        var teisu = 1
+
+        Log.v("helloWold",helloWorld)
+
+        // ${}で文字列に変数を挿入できるんです！
+        Log.v("hello","挿入 ${helloWorld}")
+
+        Log.v("teisu = ",  teisu.toString())
+
+        teisu = 2
+
+        Log.v("teisu = ", teisu.toString())
+        // for range
+        for(a in 1..10){
+                println(a)
+        }
+
+        // when(javaでいうswitch)
+        when(switch) {
+            0 -> println("aaa")
+            1 -> println("bbb")
+            2 -> println("ccc")
+        }
+
+
     }
 }
---
+++ app/src/main/java/jp/cy_world/kotaro/kotlintestapp/test.kt
@@ -0,0 +1,8 @@
+package jp.cy_world.kotaro.kotlintestapp
+
+/**
+ * Created by kotaro on 16/06/18.
+ */
+class TestClass() {
+
+}
