f1f1ec0306cc17c03e4bf776cc6b7d0dbd765355
kotaro <k.sato@st-ventures.jp>
2017-03-18 18:37:07 +0900

    TextViewに文字セットできた

--- app/src/main/java/jp/cy_world/kotaro/kotlintestapp/MainActivity.kt
+++ app/src/main/java/jp/cy_world/kotaro/kotlintestapp/MainActivity.kt
@@ -3,12 +3,17 @@ package jp.cy_world.kotaro.kotlintestapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val label: TextView = findViewById(R.id.textView) as TextView
+
+        label.text = "kotlin楽しい"
 
         // 動的に型付けるよ
         var helloWorld = "aaaaa"
